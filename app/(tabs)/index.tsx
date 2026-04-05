5f6e7d37ee7524ca90d3f96ed522b95523db60f5
Brent Edwards <bedwards@inmusicbrands.com>
2026-04-05 17:41:05 +1200

    Fix tab/index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,48 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
- 
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+const SafeAreaView = styled(RNSafeAreaView);
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Recurrly!
       </Text>
-      <Link href="/onboarding" className="mt-t rounded-md bg-primary text-white p-4">Go to onboarding</Link>
-      <Link href="/(auth)/sign-in" className="mt-t rounded-md bg-primary text-white p-4">Go to Sign In</Link>
-      <Link href="/(auth)/create-account" className="mt-t rounded-md bg-primary text-white p-4">Go to Create Account</Link>
-      <Link href="/(tabs)/subscriptions/spotify" className="mt-t rounded-md bg-primary text-white p-4">Spotify Subscription</Link>
-      <Link href={{
-        pathname: "/(tabs)/subscriptions/[id]",
-        params: { id: "claude" }
-      }} className="mt-t rounded-md bg-primary text-white p-4">Claude Subscription</Link>
-    </View>
+      <Link
+        href="/onboarding"
+        className="mt-t rounded-md bg-primary text-white p-4"
+      >
+        Go to onboarding
+      </Link>
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-t rounded-md bg-primary text-white p-4"
+      >
+        Go to Sign In
+      </Link>
+      <Link
+        href="/(auth)/create-account"
+        className="mt-t rounded-md bg-primary text-white p-4"
+      >
+        Go to Create Account
+      </Link>
+      <Link
+        href="/(tabs)/subscriptions/spotify"
+        className="mt-t rounded-md bg-primary text-white p-4"
+      >
+        Spotify Subscription
+      </Link>
+      <Link
+        href={{
+          pathname: "/(tabs)/subscriptions/[id]",
+          params: { id: "claude" },
+        }}
+        className="mt-t rounded-md bg-primary text-white p-4"
+      >
+        Claude Subscription
+      </Link>
+    </SafeAreaView>
   );
 }
